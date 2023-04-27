2353bb1220303bebe5331aacd14ab1e2862d6994
艾小君 <cl_bb_1992@163.com>
2023-04-27 03:35:08 +0000

    main

---
+++ main.py
@@ -0,0 +1,21 @@
+from fastapi import FastAPI
+
+# from controller.DataSourceApi import dataSourceApi
+# from controller.QueryDataApi import queryDataApi
+
+app = FastAPI()
+
+
+@app.get("/")
+async def root():
+    return {"message": "Hello World"}
+
+
+@app.get("/hello/{name}")
+async def say_hello(name: str):
+    return {"message": f"Hello {name}"}
+
+
+# 加载 dataSourceApi 接口
+# app.mount("/dataSource", dataSourceApi)
+# app.mount("/query", queryDataApi)
